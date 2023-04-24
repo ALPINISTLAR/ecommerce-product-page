d2400d3c2a4bd30c8b3d3b4ede25d66a392d7038
ALPINISTLAR <niveam424@gmail.com>
2023-04-24 16:46:49 +0200

    add image-showcase and image-showcase__thumbnails

--- js/main.js
+++ js/main.js
@@ -1,13 +1,40 @@
+// Shopping cart
 const elsShoppingCartRemoveButton = document.querySelectorAll('.item-shopping-cart-products__remove-button');
 const elSiteHeaderCartLink = document.querySelector('.js-site-header-cart-link');
 const elSiteHeaderCartModal = document.querySelector('.site-header__cart-modal');
 
 if (elSiteHeaderCartLink) {
   elSiteHeaderCartLink.addEventListener('click', function (evt) {
-evt.preventDefault();
-elSiteHeaderCartModal.classList.toggle('site-header__cart-modal--open');
+    evt.preventDefault();
+    elSiteHeaderCartModal.classList.toggle('site-header__cart-modal--open');
   });
 }
+
+// IMAGE SHOWCASE
+
+const elsImageShowcaseThumbnailButton = document.querySelectorAll('.js-thumbnail-button');
+const elsImageShowcaseThumbnail = document.querySelectorAll('.image-showcase__thumbnail');
+const elImageShowcaseImg = document.querySelector('.image-showcase__img');
+
+
+
+elsImageShowcaseThumbnailButton.forEach(function (elThumbnailButton) {
+  elThumbnailButton.addEventListener('click', function () {
+    elsImageShowcaseThumbnail.forEach(function (elShowcaseThumbnail) {
+      // Remove active state from all buttons
+      elShowcaseThumbnail.classList.remove('image-showcase__thumbnail--active')
+    });
+    // Add active state to clicked button
+    elThumbnailButton.parentElement.classList.add('image-showcase__thumbnail--active')
+
+    // Update active image src accordingly
+elImageShowcaseImg.src = elThumbnailButton.dataset.showcaseImgBig;
+elImageShowcaseImg.srcset = `${elThumbnailButton.dataset.showcaseImgBig} 1x, ${elThumbnailButton.dataset.showcaseImgRetina} 2x`;
+  })
+})
+
+
+
 
 
 // elsShoppingCartRemoveButton.forEach(function (elRemoveButton) {
