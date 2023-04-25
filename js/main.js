f1fbd57ff38fdb09d771711a18c10deb4660e942
ALPINISTLAR <niveam424@gmail.com>
2023-04-25 10:54:12 +0200

    add lightbox

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
+const modifiers = {
+  headerCartModalOpen: 'site-header__cart-modal--open',
+  imgThumnailActive: 'image-showcase__thumbnail--active',
+  lightboxOpen: 'lightbox--open'
+};
+
+
 // Shopping cart
 const elsShoppingCartRemoveButton = document.querySelectorAll('.item-shopping-cart-products__remove-button');
 const elSiteHeaderCartLink = document.querySelector('.js-site-header-cart-link');
@@ -6,7 +13,7 @@ const elSiteHeaderCartModal = document.querySelector('.site-header__cart-modal')
 if (elSiteHeaderCartLink) {
   elSiteHeaderCartLink.addEventListener('click', function (evt) {
     evt.preventDefault();
-    elSiteHeaderCartModal.classList.toggle('site-header__cart-modal--open');
+    elSiteHeaderCartModal.classList.toggle(modifiers.headerCartModalOpen);
   });
 }
 
@@ -16,26 +23,39 @@ const elsImageShowcaseThumbnailButton = document.querySelectorAll('.js-thumbnail
 const elsImageShowcaseThumbnail = document.querySelectorAll('.image-showcase__thumbnail');
 const elImageShowcaseImg = document.querySelector('.image-showcase__img');
 
-
-
 elsImageShowcaseThumbnailButton.forEach(function (elThumbnailButton) {
   elThumbnailButton.addEventListener('click', function () {
     elsImageShowcaseThumbnail.forEach(function (elShowcaseThumbnail) {
       // Remove active state from all buttons
-      elShowcaseThumbnail.classList.remove('image-showcase__thumbnail--active')
+      elShowcaseThumbnail.classList.remove(modifiers.imgThumnailActive)
     });
     // Add active state to clicked button
-    elThumbnailButton.parentElement.classList.add('image-showcase__thumbnail--active')
+    elThumbnailButton.parentElement.classList.add(modifiers.imgThumnailActive)
 
     // Update active image src accordingly
-elImageShowcaseImg.src = elThumbnailButton.dataset.showcaseImgBig;
-elImageShowcaseImg.srcset = `${elThumbnailButton.dataset.showcaseImgBig} 1x, ${elThumbnailButton.dataset.showcaseImgRetina} 2x`;
+    elImageShowcaseImg.src = elThumbnailButton.dataset.showcaseImgBig;
+    elImageShowcaseImg.srcset = `${elThumbnailButton.dataset.showcaseImgBig} 1x, ${elThumbnailButton.dataset.showcaseImgRetina} 2x`;
   })
 })
 
 
+// LIGHTBOX
+const elLightboxToggler = document.querySelector('.js-lightbox-toggler');
+const elLightbox = document.querySelector('.lightbox');
+const elLightboxClose = document.querySelector('.js-lightbox-close');
 
-
+// Open Lightbox
+if (elLightboxToggler) {
+  elLightboxToggler.addEventListener('click', function () {
+    elLightbox.classList.add(modifiers.lightboxOpen)
+  });
+}
+// Close Lightbox
+if (elLightboxClose) {
+  elLightboxClose.addEventListener('click', function () {
+    elLightbox.classList.remove(modifiers.lightboxOpen)
+  })
+}
 
 // elsShoppingCartRemoveButton.forEach(function (elRemoveButton) {
 // elRemoveButton.addEventListener('click', function () {
